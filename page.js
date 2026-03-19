3c8d133bd4c1dd9abe986b2db32cc42addf2dbf3
essamphysicsacademy <essamphysicsacademy@gmail.com>
2026-03-20 00:31:05 +0400

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>Essam Physics Platform 🚀</h1>
-      <p>الموقع شغال بنجاح</p>
-    </main>
-  );
-}
