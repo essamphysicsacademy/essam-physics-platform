1833efba89eefbdc4a485a0cddfae74e63ae6e34
essamphysicsacademy <essamphysicsacademy@gmail.com>
2026-03-19 06:46:51 +0400

    Delete app/app/page.js

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", direction: "rtl" }}>
-      <h1>منصة عصام للفيزياء</h1>
-      <p>ESSAM PHYSICS ACADEMY</p>
-    </main>
-  );
-}
