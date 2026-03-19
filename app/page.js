475a8d6859ed6b0a0584e622355f1611a5bb39df
essamphysicsacademy <essamphysicsacademy@gmail.com>
2026-03-19 04:43:32 +0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main style={{padding: "40px", fontFamily: "Arial", direction: "rtl"}}>
+      <h1>منصة عصام للفيزياء</h1>
+      <p>المنهج الإماراتي</p>
+      <p>ESSAM PHYSICS ACADEMY</p>
+      <p>UAE Physics Learning Platform</p>
+    </main>
+  );
+}
