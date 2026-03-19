4448a8b283533d8c15932f47ffdf48cb558dd756
essamphysicsacademy <essamphysicsacademy@gmail.com>
2026-03-20 01:09:25 +0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>Essam Physics Platform 🚀</h1>
+      <p>الموقع اشتغل خلاص 🔥</p>
+    </div>
+  );
+}
